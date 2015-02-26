ea05ed1d7712c594de693445e22e7e74845d96db
Dave Arter <dave@minus-zero.org>
2015-02-26 21:21:19 +0000

    Add a punchcard display to show busy/quiet periods
    Also refactors things a bit to reduce code duplication

--- hubbub.py
+++ hubbub.py
@@ -3,7 +3,11 @@ import time
 import os
 from operator import attrgetter
 from itertools import islice
-from datetime import date
+from datetime import date, datetime
+from collections import defaultdict, Counter
+from math import floor
+import colorsys
+from random import randint
 
 import unicornhat
 from github3 import login
@@ -19,18 +23,95 @@ ROWS = 8
 
 class BaseView(object):
     max_events = COLS * ROWS
+    today_only = False
+
+    def __init__(self, manager):
+        self.manager = manager
+        self.events = []
 
     def add_events(self, events):
-        pass
+        if self.today_only:
+            today = date.today()
+            events = [
+                e
+                for e
+                in events
+                if e.created_at.date() == today
+            ]
+        for event in events:
+            self.add_event(event)
+
+    def add_event(self, event):
+        self.events.append(event)
 
     def render(self):
         pass
 
 
+class PunchcardView(BaseView):
+    """
+    Displays recent activity in a similar manner to GitHub's punch card, e.g.:
+    https://github.com/mysociety/pombola/graphs/punch-card
+    Each row represents one wallclock-hour of activity, divided into as
+    many equal blocks as there are columns.
+    The most recent hour is at the bottom.
+    """
+    max_events = None
+    today_only = True
+    hue = None
+
+    def __init__(self, *args, **kwargs):
+        super(PunchcardView, self).__init__(*args, **kwargs)
+        self.hue = randint(0, 359)
+
+    def _get_punchcard(self):
+        punchcard = defaultdict(Counter)
+        for event in self.events:
+            created_at = event.created_at
+            hour = created_at.hour
+            minute = created_at.minute + (created_at.second / 60.0)
+            x = int(floor(minute/(60.0/COLS)))
+            punchcard[hour][x] += 1
+        return punchcard
+
+    def render(self):
+        print "PunchcardView.render:"
+        punchcard = self._get_punchcard()
+        max_value = max(v for row in punchcard.values() for v in row.values())
+        max_value = float(max_value)
+        end = datetime.utcnow().hour + 1
+        start = end - ROWS
+        # Boost pixel brightness by this much so quiet times are still visible
+        boost = 0.075
+        unicornhat.clear()
+        # Do a bit of colour cycling to indicate an update
+        new_hue = randint(0, 359)
+        for hue in range(min(self.hue, new_hue), max(self.hue, new_hue)+1):
+            for hour in range(start, end):
+                y = hour - start
+                for x in range(COLS):
+                    h = hue / 360.0
+                    s = 1.0
+                    # Cap lightness so the brightest pixel isn't quite white
+                    v = min(0.8, punchcard[hour][x] / max_value)
+                    # Give the value a little boost if it's above 0
+                    if v > 0:
+                        v = (v * (1 - boost)) + boost
+                    # Convert our 0-1 HSL values into 0-255 RGB for display
+                    r, g, b = colorsys.hls_to_rgb(h, v, s)
+                    r, g, b = (int(round(i * 255)) for i in (r, g, b))
+                    unicornhat.set_pixel(x, y, r, g, b)
+            unicornhat.show()
+        self.hue = new_hue
+        print "   done."
+
+
 class StreamView(BaseView):
-    def __init__(self, manager):
-        self.manager = manager
-        self.events = []
+    """
+    Renders events in a stream across the display, which slowly
+    scrolls upwards as new events occur.
+    """
+    today_only = True
 
     def _parse_colour(self, colour):
         """Parse a CSS colour into a unicornhat-friendly (r, g, b) tuple"""
@@ -85,32 +166,21 @@ class StreamView(BaseView):
             r, g, b = [min(255, int(round(v*1.5))) for v in (r, g, b)]
         return r, g, b
 
-    def add_events(self, events):
-        today = date.today()
-        filtered_events = [
-            e
-            for e
-            in events[-self.max_events:]
-            if e.created_at.date() == today
-        ]
-        for event in filtered_events:
-            self.add_event(event)
-        print "Added {} new events to StreamView".format(len(filtered_events))
-
     def add_event(self, event):
+        # If the display is full up, chop a line off
+        # the top to make space at the bottom for new events
         if len(self.events) >= ROWS * COLS:
             new_length = COLS * (ROWS - 1)
             self.events = self.events[-new_length:]
-        self.events.append(event)
-        self.events.sort(key=attrgetter('created_at'))
+        return super(StreamView, self).add_event(event)
 
     def render(self):
-        if not self.events:
-            return
-        print "Rendering events:"
+        print "StreamView.render:"
         unicornhat.clear()
-        count = ROWS * COLS
-        for i, event in enumerate(self.events[-count:]):
+        # Because we render one event per-pixel, we need to only
+        # render the most recent self.max_events events
+        start = self.max_events * -1
+        for i, event in enumerate(self.events[start:]):
             x = i % COLS
             y = i // ROWS
             r, g, b = self._event_colour(event)
@@ -126,7 +196,7 @@ class EventsManager(object):
     def __init__(self, config, user):
         self.config = config
         self.user = user
-        self.view = StreamView(manager=self)
+        self.view = PunchcardView(manager=self)
         self.all_events = []
         self.seen_event_ids = set()
         org = self.config.GITHUB_ORG
@@ -144,6 +214,7 @@ class EventsManager(object):
         except RequestException:
             print "   failed."
             return []
+        new_events.sort(key=attrgetter('created_at'))
         self.all_events += new_events
         self.seen_event_ids.update(set([e.id for e in new_events]))
         self.events_iterator.refresh(True)
